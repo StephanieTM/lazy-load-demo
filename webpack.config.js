fac6e92dc4efefd849413fb3ae4512a7a661096a
Stephanie.Gao <tm1997@163.com>
2021-08-24 21:43:15 +0800

    feat: init project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const resolvePath = x => path.resolve(__dirname, ...x.split(path.sep));
+
+module.exports = {
+  entry: {
+    main: resolvePath('src/app.js'),
+  },
+  output: {
+    path: resolvePath('public'),
+    uniqueName: 'main',
+    clean: true,
+  },
+  mode: 'development',
+  bail: true,
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolvePath('src/index.ejs'),
+    }),
+  ],
+};
